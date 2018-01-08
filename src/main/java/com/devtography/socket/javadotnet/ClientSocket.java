d0d29533d90b363e4e61dfda7c7a3af2530eb994
Wing Chau <wing@devtography.com>
2018-01-08 05:21:22 +0000

    Added end of stream reached check
    Check the total number of bytes read into the buffer equals -1 or not to
    prevent -1 being passes to as the length value when forming a new
    String.

--- src/main/java/com/devtography/socket/javadotnet/ClientSocket.java
+++ src/main/java/com/devtography/socket/javadotnet/ClientSocket.java
@@ -147,7 +147,8 @@ public class ClientSocket {
         while (msg.lastIndexOf(delimiter) == -1) {
             int bytesRead = in.read(buffer);
 
-            msg.append(new String(buffer, 0, bytesRead));
+            if (bytesRead > -1)
+                msg.append(new String(buffer, 0, bytesRead));
         }
 
         // passes message string to listener without
